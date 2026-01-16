8e3bc73d08e5ea16ccaec99d8f6e8bec57e2009e
Thenuwara <thenuwaradb@gmail.com>
2026-01-15 19:57:19 -0500

    Changed redirect on homepage

--- app/page.js
+++ app/page.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-black px-6">
       <div className="max-w-xl text-center -translate-y-12">
         <h1 className="text-4xl font-bold text-yellow-400 mb-3 tracking-normal">UW Lost and Found </h1>
         <p className="text-lg font-medium text-gray-100 mb-4"> Tired of losing items and having nowhere to look? </p>
-        <Link href="/student/signup" className="inline-block bg-yellow-400 text-black font-bold px-4 py-2 rounded-xl shadow-lg hover:transform transition-transform duration-200 hover:scale-105">
+        <Link href="/student/" className="inline-block bg-yellow-400 text-black font-bold px-4 py-2 rounded-xl shadow-lg hover:transform transition-transform duration-200 hover:scale-105">
           Get Started
         </Link>
       </div>
