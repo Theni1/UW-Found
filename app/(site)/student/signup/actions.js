f70aa8f52c9bba8ef43214e463d02461f46c382c
Thenuwara <thenuwaradb@gmail.com>
2026-03-26 21:19:16 -0400

    Fixed auth issue

--- app/(site)/student/signup/actions.js
+++ app/(site)/student/signup/actions.js
@@ -1,15 +1,22 @@
 "use server"
 import { createClient } from "@/utils/supabase/server";
 
-export async function signUpStudent (formData) {
+export async function signUpStudent (prevState, formData) {
     const supabase = await createClient();
     const email = String(formData.get("email"))
     const password = String(formData.get("password"))
-    const {} = await supabase.auth.signUp({
+    const {error} = await supabase.auth.signUp({
         email: email,
         password: password,
         options: {
-            emailRedirectTo: "http://localhost:3000/auth/callback?next=/student/login",
+            emailRedirectTo: "https://uw-found.vercel.app/auth/callback?next=/student/login",
         },
     });
+    
+    if (error) {
+        console.log("Supabase signUp error:", error);
+        return { success: false, error: error.message };
+    }
+    return { success: true, error: null };
+
 }
--- app/(site)/student/signup/page.js
+++ app/(site)/student/signup/page.js
@@ -1,19 +1,24 @@
 "use client"
 
 import {signUpStudent} from "./actions.js"
+import {useFormState } from "react-dom";
+
+const initialState = { success: false, error: null };
 
 export default function StudentPage () {
+  const [state, formAction] = useFormState(signUpStudent, initialState);
  
   return (
-    <div className="min-h-[calc(100vh-5rem)] flex justify-center items-center bg-white text-black">
-      <form action={signUpStudent} className="flex flex-col gap-2 w-full max-w-sm mx-auto border bg-white border-white rounded-xl p-6 shadow-lg">
-      <h1 className = "mb-4 text-xl font-bold text-center tracking-wide">Sign up</h1>
-      <label className = "text-sm font-bold">Email</label>
-      <input name = "email" type = "email" className = "border border-gray-600 bg-black rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-yellow-400"/>
-      <label className = "text-sm font-bold">Password</label>
-      <input name = "password" type = "password" className = "border border-gray-600 bg-black text-gray-100 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-yellow-400"/>
-      <button type = "submit" className = "self-center cursor-pointer bg-yellow-400 text-black font-bold px-4 py-2 rounded-xl shadow-lg hover:transform transition-transform duration-200 hover:scale-105 mt-4">Submit</button>
-    </form>
+    <div className = "min-h-[calc(100vh-5rem)] flex justify-center items-center bg-white text-black">
+        <form action = {formAction} className = "flex flex-col gap-2 w-full max-w-sm mx-auto border border-white rounded-xl p-6 shadow-lg">
+          <h1 className = "mb-4 text-xl font-bold text-center tracking-wide">Sign up</h1>
+          <label className = "text-sm font-bold">Email</label>
+          <input name = "email" type = "email" className = "border border-gray-600 bg-black text-gray-100 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-yellow-400"/>
+          <label className = "text-sm font-bold">Password</label>
+          <input name = "password" type = "password" className = "border border-gray-600 bg-black text-gray-100 rounded-md p-1 focus:outline-none focus:ring-2 focus:ring-yellow-400"/>
+          {state.success ? <p className = "text-lg pt-5 font-bold text-center text-green-500">Email sent!</p> : <p className = "text-red-700">{state.error}</p>}
+          <button className = "self-center cursor-pointer bg-yellow-400 text-black font-bold px-4 py-2 rounded-xl shadow-lg hover:transform transition-transform duration-200 hover:scale-105 mt-4" type = "submit">Submit</button>
+        </form>
     </div>  
   );
 }
